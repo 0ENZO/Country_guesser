543ca51bf50664e4b91de5d084f2302c41d0e17f
0ENZO <Enzo.arhab@gmail.com>
2021-08-12 20:25:39 +0200

    init library

---
+++ mlp_library/cmake-build-debug/CMakeFiles/mlp_library.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mlp_library.dir/main.cpp.obj"
+  "mlp_library.exe"
+  "mlp_library.exe.manifest"
+  "mlp_library.lib"
+  "mlp_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mlp_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
